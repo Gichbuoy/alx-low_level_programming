f08e245db0bfdd89dddd819be29b86112b0248ea
Alex Maina <lexizgichbuoy@gmail.com>
2023-03-22 13:37:13 -0700

    int_index

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * is_98 - check if a number is equal to 98
+ * is_98 - check if a number is equals to 98
  * @elem: the integer to check
  *
  * Return: 0 if false, something else otherwise.
