387a5941ad7572ea07ecf3d82d184b271a46e012
Alex Maina <lexizgichbuoy@gmail.com>
2023-03-22 12:58:55 -0700

    main.c_function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,10 +4,10 @@
 #include "3-calc.h"
 /**
  * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers to the arguments
  *
- * Return: Always 0.
+ * Return: Always zero(0)
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
