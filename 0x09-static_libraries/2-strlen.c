484e4d266ccbd1ec50318d16e1edfa24bffc7031
Alex Maina <lexizgichbuoy@gmail.com>
2023-03-31 09:16:34 -0700

    string_length

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
-   * _strlen - Function for counting the length of string
-    * @s: a pointer for counting the strting length
-     * Return: int i
+ * _strlen - Function for counting the length of string
+ * @s: a pointer for counting the strting length
+ * Return: int i
 */
 
 int _strlen(char *s)
