113ff2dde13edc3141640460b880b747e7c5349a
Alex Maina <lexizgichbuoy@gmail.com>
2023-04-01 11:13:01 -0700

    sub_string

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -2,10 +2,10 @@
 #define NULL 0
 
 /**
-  * _strstr - locate and return pointer to first occurence of substring
+  * _strstr - locate and return pointer to first occurence of a substring.
   * @haystack: string to search
   * @needle: target substring to search for
-  * Return: pointer to index of string at first occurence of whole substring
+  * Return: pointer to index of string at first occurence of the whole substring
 */
 char *_strstr(char *haystack, char *needle)
 {
