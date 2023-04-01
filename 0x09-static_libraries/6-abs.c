2f03b28b1e1809a464ce366136e117408ed20513
Alex Maina <lexizgichbuoy@gmail.com>
2023-04-01 11:19:17 -0700

    absolute_value

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
-   * _abs - computes the absolute value of an integer
-    * @n: The int to be operated upon
-     * Return: returns an unsigneed int value to calling function
-*/
+ * _abs - computes the absolute value of an integer (n)
+ * @n: The int to be operated upon
+ * Return: returns an unsigneed int value to calling function
+ */
 int _abs(int n)
 {
 	if (n > 0)
